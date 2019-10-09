60785e3a5ac55693799384cb47cf972d980ecbd8
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-09 16:22:57 -0300

    Start project - Integer specific service.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.services.CalculationService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
 
+		List<Integer> list = new ArrayList<>();
+
+		String pathIn = "C:\\Temp\\In\\inteiros.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(pathIn))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				list.add(Integer.parseInt(line));
+				line = br.readLine();
+			}
+			Integer x = CalculationService.max(list);
+			System.out.println("Max:");
+			System.out.println(x);
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
 	}
 
 }
